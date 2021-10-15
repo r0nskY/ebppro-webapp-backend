600c2c031debb8c2b70bed0053422eea03820482
r0nskY <rondellel05@gmail.com>
2021-10-15 16:51:27 +0800

    first commit

---
+++ src/main/java/com/example/ebppro_webapp/EBpProWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebppro_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBpProWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBpProWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ebppro_webapp/EBpProWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebppro_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBpProWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
